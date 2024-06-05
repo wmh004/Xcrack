b24ef9d42d4facdecb8bbdea897b03a15b974945
Wan <148860899+wmh004@users.noreply.github.com>
2024-06-05 20:09:26 +0800

    Add files via upload

---
+++ Xcrack/Backend/src/main/java/com/example/Xcrack/XcrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.Xcrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcrackApplication.class, args);
+	}
+
+}
---
+++ Xcrack/Backend/src/test/java/com/example/Xcrack/XcrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Xcrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
