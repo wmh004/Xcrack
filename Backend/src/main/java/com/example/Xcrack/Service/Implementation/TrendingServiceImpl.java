68ec70c665f5f101948ff1b59df040a0a8881ec9
unknown <syahmieandy@gmail.com>
2024-06-11 03:55:25 +0800

    add increment for existing hashtag method

--- Backend/src/main/java/com/example/Xcrack/Service/Implementation/TrendingServiceImpl.java
+++ Backend/src/main/java/com/example/Xcrack/Service/Implementation/TrendingServiceImpl.java
@@ -4,7 +4,9 @@ package com.example.Xcrack.Service.Implementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.Xcrack.Repository.TrendingHashtagRepository;
+import com.example.Xcrack.Repository.PostRepository;
 import com.example.Xcrack.Model.Hashtag;
+import com.example.Xcrack.Model.Post;
 import com.example.Xcrack.Service.TrendingService;
 
 
@@ -16,9 +18,27 @@ public class TrendingServiceImpl implements TrendingService {
     @Autowired
     private TrendingHashtagRepository hashtagRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public List<Hashtag> getTopTrendingHashtags(int limit) {
         return hashtagRepository.findTopTrendingHashtags(limit);
 
 
-}}
+}
+     @Override
+    public void addPost(Post post) {
+        for (Hashtag hashtag : post.getHashtags()) {
+            Hashtag existingHashtag = hashtagRepository.findByHashtag(hashtag.getHashtag());
+            if (existingHashtag != null) {
+                existingHashtag.incrementCount();
+                hashtagRepository.save(existingHashtag);
+            } else {
+                hashtag.setCount(1);
+                hashtagRepository.save(hashtag);
+            }
+        }
+        postRepository.save(post);
+    }
+}
