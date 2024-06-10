e3f4d9c866392a89228219ebafdbab25627608d5
unknown <syahmieandy@gmail.com>
2024-06-11 03:54:06 +0800

    minor change in trendingService

--- Backend/src/main/java/com/example/Xcrack/Service/TrendingService.java
+++ Backend/src/main/java/com/example/Xcrack/Service/TrendingService.java
@@ -1,8 +1,11 @@
 package com.example.Xcrack.Service;
 
 import com.example.Xcrack.Model.Hashtag;
+import com.example.Xcrack.Model.Post;
+
 import java.util.List;
 
 public interface TrendingService {
     List<Hashtag> getTopTrendingHashtags(int limit);
+    void addPost(Post post);
 }
